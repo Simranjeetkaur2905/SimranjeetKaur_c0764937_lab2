8120dc2cbcf4684c91bf49ad9ea25efc05a2265b
maan-satveer <koursatveer12@gmail.com>
2020-01-21 18:17:01 -0500

    Initial Commit

---
+++ SimranjeetKaur_c0764937_lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimranjeetKaur_c0764937_lab2
+//
+//  Created by Simran Chakkal on 2020-01-21.
+//  Copyright © 2020 simran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
